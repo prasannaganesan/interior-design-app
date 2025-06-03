70e764443fe3ec60f9c4d55df2e8c1d0d02047e9
prasannaganesan <pg@machinify.com>
2025-06-02 22:05:26 -0700

    fix: disable modulePreload polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,10 @@ export default defineConfig({
       'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp'
     }
+  },
+  build: {
+    modulePreload: {
+      polyfill: false
+    }
   }
 })
